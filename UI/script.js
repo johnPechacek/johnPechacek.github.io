3d4395b51d059d425a6624766473b469b3f8680c
johnPechacek <johnapechacek@gmail.com>
2024-06-14 14:31:26 -0500

    added lotties back in, implemented interactions

--- UI/script.js
+++ UI/script.js
@@ -15,3 +15,62 @@ LottieInteractivity.create({
         },
     ],
 });
+
+LottieInteractivity.create({
+    mode: 'chain',
+    player: '#lottie-trophy',
+    actions: [
+        {
+            state: 'loop',
+            transition: 'click',
+            frames: 'trophy-empty',
+        },
+        {
+            state: 'autoplay',
+            transition: 'onComplete',
+            frames: 'trophy-animate',
+        },
+        {
+            state: 'loop',
+            transition: 'click',
+            frames: 'trophy-filled',
+            reset: true
+        },
+    ],
+});
+
+LottieInteractivity.create({
+    mode: 'chain',
+    player: '#lottie-waveform',
+    actions: [
+        {
+            state: 'loop',
+            transition: 'click',
+            frames: 'waveform-idle',
+        },
+        {
+            state: 'loop',
+            transition: 'click',
+            frames: 'waveform-loop',
+            reset: true
+        },
+    ],
+});
+
+LottieInteractivity.create({
+    mode: 'chain',
+    player: '#lottie-heart',
+    actions: [
+        {
+            state: 'autoplay',
+            transition: 'click',
+            frames: 'idle',
+        },
+        {
+            state: 'autoplay',
+            transition: 'click',
+            frames: 'active',
+            reset: true
+        },
+    ],
+});
