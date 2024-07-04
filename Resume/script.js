e3692276d6974234774f6fa6f86416084ff09dd2
johnPechacek <johnapechacek@gmail.com>
2024-07-04 01:00:18 -0500

    implemented javascript for mobile burger menu

--- Resume/script.js
+++ Resume/script.js
@@ -1,21 +1,3 @@
-LottieInteractivity.create({
-    mode: 'chain',
-    player: '#lottie-chest-homepage',
-    actions: [
-        {
-            state: 'loop',
-            transition: 'click',
-            frames: 'rewards-chest-idle',
-        },
-        {
-            state: 'autoplay',
-            transition: 'onComplete',
-            frames: 'rewards-chest-clicked-4',
-            reset: true
-        },
-    ],
-});
-
 const burgerMenuBtn = document.querySelector('.hamburger-menu-btn');
 const burgerMenuIcon = document.querySelector('.hamburger-menu-icon');
 const burgerMenu = document.querySelector('.hamburger-menu');
--- UI/script.js
+++ UI/script.js
@@ -74,3 +74,21 @@ LottieInteractivity.create({
         },
     ],
 });
+
+const burgerMenuBtn = document.querySelector('.hamburger-menu-btn');
+const burgerMenuIcon = document.querySelector('.hamburger-menu-icon');
+const burgerMenu = document.querySelector('.hamburger-menu');
+
+let menuOpen = false;
+
+burgerMenuBtn.addEventListener('click', () => {
+    if (menuOpen) {
+        burgerMenuIcon.classList.remove('hamburger-menu-icon-x');
+        burgerMenu.classList.remove('hamburger-menu-down');
+        menuOpen = false;
+    } else {
+        burgerMenuIcon.classList.add('hamburger-menu-icon-x');
+        burgerMenu.classList.add('hamburger-menu-down');
+        menuOpen = true;
+    }
+});
